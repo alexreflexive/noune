77e35a67d8251cad8ebf7026735f82922089e9e3
Alejandro GABRIEL Y GALAN <alexgyg@msn.com>
2025-05-08 22:53:03 +0200

    Commit pour que Grok puisse voir 1.

--- GameEngine/controls.js
+++ GameEngine/controls.js
@@ -1,9 +1,38 @@
 export const Controls = () => {
+  const ctrlBtn1 = document.querySelector("#ctrl-btn-1");
+  const ctrlBtn2 = document.querySelector("#ctrl-btn-2"); 
+  const ctrlBtn3 = document.querySelector("#ctrl-btn-3");
+  const btnMin = document.querySelector("#btn-min");
+  const btnMax = document.querySelector("#btn-max");
+
+  ctrlBtn1.addEventListener("click", function () {
+      console.log("Button 1 clicked");
+    });
+
+  ctrlBtn2.addEventListener("click", function () {
+    console.log("Button 2 clicked");
+  });
+
+  ctrlBtn3.addEventListener("click", function () {
+    console.log("Button 3 clicked");
+  });
+
+  btnMin.addEventListener("click", function () {
+      console.log("Button Min clicked");
+    });
+
+  btnMax.addEventListener("click", function () {
+      console.log("Button Max clicked");
+    });
+
+
   return {
     addMessage: function (newMessage) {
       console.log("controls " + newMessage + "\n");
       const msgBox = document.querySelector("#messagebox");
       msgBox.innerHTML = newMessage + "<br/>" + msgBox.innerHTML;
     },
+
+
   };
 };
--- notes.md
+++ notes.md
@@ -8,3 +8,8 @@ Dépôt GitHub : https://github.com/alexreflexive/noune.git
 # GARDER A L'ESPRIT
 
 - Les messages dans la messageBox doivent à terme partir depuis index.js et non depuis dialog.js.
+
+# Règles pour les messages lors des commits
+On distingue bien ce que relève du graphisme, le moteur de jeu, de ce qui relève de la logique de jeu.
+Les premiers sont notés "GAMEENGINE : bla bla bla" et les seconds "LOGIC : bla bla bla."
+Plus tard on distinguera également ce qui relève d'une modification, d'un ajout minime et du débogage.
